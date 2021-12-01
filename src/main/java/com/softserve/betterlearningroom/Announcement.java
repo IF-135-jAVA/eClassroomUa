04674d99e4319b0c4f9ee3a88028048f4c6a696a
Oksana <oksanamow@gmail.com>
2021-12-01 15:52:56 +0200

    add class Announcement and class Comment

---
+++ src/main/java/com/softserve/betterlearningroom/Announcement.java
@@ -0,0 +1,59 @@
+package com.softserve.betterlearningroom;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Announcement {
+    private long id;
+    private String text;
+    private List<Comment> comments;
+
+    public Announcement() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Announcement that = (Announcement) o;
+        return id == that.id &&
+                Objects.equals(text, that.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, text);
+    }
+
+    @Override
+    public String toString() {
+        return "Announcement{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/softserve/betterlearningroom/Comment.java
@@ -0,0 +1,70 @@
+package com.softserve.betterlearningroom;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Comment {
+    private long id;
+    private User author;
+    private String text;
+    private LocalDateTime date;
+
+    public Comment() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return id == comment.id &&
+                Objects.equals(text, comment.text) &&
+                Objects.equals(date, comment.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, text, date);
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
