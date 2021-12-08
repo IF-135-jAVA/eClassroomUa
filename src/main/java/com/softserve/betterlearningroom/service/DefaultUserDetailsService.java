d8d33defc07954a69f86d03b1df9ade5479c1db0
ykotsiuba <jurok3x@gmail.com>
2021-12-08 16:32:32 +0200

    Added UserDetails and UserDetailsService.

---
+++ src/main/java/com/softserve/betterlearningroom/service/DefaultUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.softserve.betterlearningroom.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.softserve.betterlearningroom.dao.UserDao;
+import com.softserve.betterlearningroom.entity.CustomUserDetails;
+import com.softserve.betterlearningroom.entity.User;
+
+public class DefaultUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	UserDao userDao;
+
+	@Override
+	public CustomUserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userDao.findByEmail(email).orElseThrow(
+				() -> new UsernameNotFoundException(String.format("User with email - %s, not found", email)));
+		return CustomUserDetails.userToCustomUserDetails(user);
+	}
+
+}
