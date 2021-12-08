7fe9d6822b9d50814a09d17123407a2a7ae3f523
Oksana <oksanamow@gmail.com>
2021-12-08 15:50:17 +0200

    modify file

--- src/main/java/com/softserve/betterlearningroom/dto/AnnouncementDTO.java
+++ src/main/java/com/softserve/betterlearningroom/dto/AnnouncementDTO.java
@@ -1,61 +1,66 @@
 package com.softserve.betterlearningroom.dto;
 
 import com.softserve.betterlearningroom.entity.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.Objects;
-
+@Data
+//@AllArgsConstructor
+@NoArgsConstructor
 public class AnnouncementDTO {
     private long id;
     private String text;
     private List<Comment> comments;
 
-    public AnnouncementDTO() {
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public List<Comment> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<Comment> comments) {
-        this.comments = comments;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AnnouncementDTO that = (AnnouncementDTO) o;
-        return id == that.id &&
-                Objects.equals(text, that.text);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, text);
-    }
-
-    @Override
-    public String toString() {
-        return "Announcement{" +
-                "id=" + id +
-                ", text='" + text + '\'' +
-                '}';
-    }
+//    public AnnouncementDTO() {
+//    }
+//
+//    public long getId() {
+//        return id;
+//    }
+//
+//    public void setId(long id) {
+//        this.id = id;
+//    }
+//
+//    public String getText() {
+//        return text;
+//    }
+//
+//    public void setText(String text) {
+//        this.text = text;
+//    }
+//
+//    public List<Comment> getComments() {
+//        return comments;
+//    }
+//
+//    public void setComments(List<Comment> comments) {
+//        this.comments = comments;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        AnnouncementDTO that = (AnnouncementDTO) o;
+//        return id == that.id &&
+//                Objects.equals(text, that.text);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(id, text);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Announcement{" +
+//                "id=" + id +
+//                ", text='" + text + '\'' +
+//                '}';
+//    }
 }
--- src/main/java/com/softserve/betterlearningroom/entity/Announcement.java
+++ src/main/java/com/softserve/betterlearningroom/entity/Announcement.java
@@ -1,59 +1,65 @@
 package com.softserve.betterlearningroom.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 import java.util.Objects;
 
+@Data
+//@AllArgsConstructor
+@NoArgsConstructor
 public class Announcement {
     private long id;
     private String text;
     private List<Comment> comments;
 
-    public Announcement() {
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public List<Comment> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<Comment> comments) {
-        this.comments = comments;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Announcement that = (Announcement) o;
-        return id == that.id &&
-                Objects.equals(text, that.text);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, text);
-    }
-
-    @Override
-    public String toString() {
-        return "Announcement{" +
-                "id=" + id +
-                ", text='" + text + '\'' +
-                '}';
-    }
+//    public Announcement() {
+//    }
+//
+//    public long getId() {
+//        return id;
+//    }
+//
+//    public void setId(long id) {
+//        this.id = id;
+//    }
+//
+//    public String getText() {
+//        return text;
+//    }
+//
+//    public void setText(String text) {
+//        this.text = text;
+//    }
+//
+//    public List<Comment> getComments() {
+//        return comments;
+//    }
+//
+//    public void setComments(List<Comment> comments) {
+//        this.comments = comments;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Announcement that = (Announcement) o;
+//        return id == that.id &&
+//                Objects.equals(text, that.text);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(id, text);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Announcement{" +
+//                "id=" + id +
+//                ", text='" + text + '\'' +
+//                '}';
+//    }
 }
