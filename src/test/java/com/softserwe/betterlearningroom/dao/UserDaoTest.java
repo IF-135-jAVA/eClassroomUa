ff3246b7f19e268f15a4ec87885b1da5891f7b25
ykotsiuba <jurok3x@gmail.com>
2021-12-08 16:37:09 +0200

    Introduced UserDaoTest

--- src/test/java/com/softserwe/betterlearningroom/dao/UserDaoTest.java
+++ src/test/java/com/softserwe/betterlearningroom/dao/UserDaoTest.java
@@ -1,30 +1,16 @@
 package com.softserwe.betterlearningroom.dao;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Optional;
 
 import javax.sql.DataSource;
 
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.ResultSetExtractor;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import com.softserve.betterlearningroom.dao.UserDao;
 import com.softserve.betterlearningroom.dao.impl.UserDaoImpl;
@@ -48,7 +34,6 @@ public class UserDaoTest {
 		UserDao userDao = new UserDaoImpl(template, userMapper);
 		User user = new User(1, "Keanu", "Reeves", "1234", "Keanu@gmail.com", true);
 		assertEquals(Optional.of(user), userDao.findById(1));
-		// assertEquals("Keanu@gmail.com", userDao.findById(1).get().getEmail());
 	}
 	
 }
