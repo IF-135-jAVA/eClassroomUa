dc6c2763d02b423b179c8d29e5a95bc74447673c
ykotsiuba <jurok3x@gmail.com>
2021-12-09 11:07:06 +0200

    Added UserDetailsService, SecurityConfiguration, JwtProvider

--- src/main/java/com/softserve/betterlearningroom/service/CustomUserDetailsService.java
+++ src/main/java/com/softserve/betterlearningroom/service/CustomUserDetailsService.java
@@ -3,12 +3,14 @@ package com.softserve.betterlearningroom.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.softserve.betterlearningroom.dao.UserDao;
 import com.softserve.betterlearningroom.entity.CustomUserDetails;
 import com.softserve.betterlearningroom.entity.User;
 
-public class DefaultUserDetailsService implements UserDetailsService {
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
 
 	@Autowired
 	UserDao userDao;
