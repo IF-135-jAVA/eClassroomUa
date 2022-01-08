78fddc377ab363c71ddec23a4cd0be125d305487
ykotsiuba <jurok3x@gmail.com>
2022-01-08 18:51:58 +0200

    Updated test-data.sql.

--- src/main/resources/db/users/test-data.sql
+++ src/main/resources/db/users/test-data.sql
@@ -1,4 +1,8 @@
-INSERT INTO users(id, firstname, lastname, email, password, enabled) VALUES (1, 'Keanu', 'Reeves', 'Keanu@gmail.com', '$2a$04$MzVXtd4o0y4DOlyHMMLMDeE4/eezrsT5Xad.2lmGr/NkCpwBgvn3e', true);
-INSERT INTO users(id, firstname, lastname, email, password, enabled) VALUES (2, 'Yurii', 'Kotsiuba', 'jurok3x@gmail.com', '$2a$04$MzVXtd4o0y4DOlyHMMLMDeE4/eezrsT5Xad.2lmGr/NkCpwBgvn3e', true);
-INSERT INTO users(id, firstname, lastname, email, password, enabled) VALUES (3, 'John', 'Smith', 'jsmith@gmail.com', '$2a$04$MzVXtd4o0y4DOlyHMMLMDeE4/eezrsT5Xad.2lmGr/NkCpwBgvn3e', true);
-INSERT INTO users(id, firstname, lastname, email, password, enabled) VALUES (4, 'John', 'Doe', 'Keanu@gmail.com', '$2a$04$MzVXtd4o0y4DOlyHMMLMDeE4/eezrsT5Xad.2lmGr/NkCpwBgvn3e', true);
+MERGE INTO users(id, firstname, lastname, email, password, enabled)
+VALUES (1, 'Keanu', 'Reeves', 'Keanu@gmail.com', '$2a$04$MzVXtd4o0y4DOlyHMMLMDeE4/eezrsT5Xad.2lmGr/NkCpwBgvn3e', true);
+MERGE INTO users(id, firstname, lastname, email, password, enabled)
+VALUES (2, 'Yurii', 'Kotsiuba', 'jurok3x@gmail.com', '$2a$04$MzVXtd4o0y4DOlyHMMLMDeE4/eezrsT5Xad.2lmGr/NkCpwBgvn3e', true);
+MERGE INTO users(id, firstname, lastname, email, password, enabled)
+VALUES (3, 'John', 'Smith', 'jsmith@gmail.com', '$2a$04$MzVXtd4o0y4DOlyHMMLMDeE4/eezrsT5Xad.2lmGr/NkCpwBgvn3e', true);
+MERGE INTO users(id, firstname, lastname, email, password, enabled)
+VALUES (4, 'John', 'Doe', 'Keanu@gmail.com', '$2a$04$MzVXtd4o0y4DOlyHMMLMDeE4/eezrsT5Xad.2lmGr/NkCpwBgvn3e', true);
