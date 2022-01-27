f725afaba347bc35637d20b2cbcf521da22e28b8
Oksana <oksanamow@gmail.com>
2022-01-27 16:41:31 +0200

    modify mapping, add annotation PreAuthorize

--- src/main/java/com/softserve/betterlearningroom/entity/roles/Roles.java
+++ src/main/java/com/softserve/betterlearningroom/entity/roles/Roles.java
@@ -8,22 +8,7 @@ import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static com.softserve.betterlearningroom.entity.roles.Permissions.ASSIGNMENT_READ;
-import static com.softserve.betterlearningroom.entity.roles.Permissions.ASSIGNMENT_WRITE;
-import static com.softserve.betterlearningroom.entity.roles.Permissions.CLASSROOM_READ;
-import static com.softserve.betterlearningroom.entity.roles.Permissions.CLASSROOM_WRITE;
-import static com.softserve.betterlearningroom.entity.roles.Permissions.COMMENT_READ;
-import static com.softserve.betterlearningroom.entity.roles.Permissions.COMMENT_WRITE;
-import static com.softserve.betterlearningroom.entity.roles.Permissions.CRITERION_READ;
-import static com.softserve.betterlearningroom.entity.roles.Permissions.CRITERION_WRITE;
-import static com.softserve.betterlearningroom.entity.roles.Permissions.LEVEL_READ;
-import static com.softserve.betterlearningroom.entity.roles.Permissions.LEVEL_WRITE;
-import static com.softserve.betterlearningroom.entity.roles.Permissions.TOPIC_READ;
-import static com.softserve.betterlearningroom.entity.roles.Permissions.TOPIC_WRITE;
-import static com.softserve.betterlearningroom.entity.roles.Permissions.USER_ASSIGNMENT_READ;
-import static com.softserve.betterlearningroom.entity.roles.Permissions.USER_ASSIGNMENT_WRITE;
-import static com.softserve.betterlearningroom.entity.roles.Permissions.USER_READ;
-import static com.softserve.betterlearningroom.entity.roles.Permissions.USER_WRITE;
+import static com.softserve.betterlearningroom.entity.roles.Permissions.*;
 
 @Getter
 @RequiredArgsConstructor
@@ -31,10 +16,12 @@ public enum Roles {
 
     TEACHER(Arrays.asList(ASSIGNMENT_READ, ASSIGNMENT_WRITE, USER_READ, USER_WRITE, CLASSROOM_READ, CLASSROOM_WRITE,
             TOPIC_READ, TOPIC_WRITE, USER_ASSIGNMENT_READ, USER_ASSIGNMENT_WRITE, COMMENT_READ, COMMENT_WRITE,
-            CRITERION_READ, CRITERION_WRITE, LEVEL_READ, LEVEL_WRITE).stream().collect(Collectors.toSet())),
+            CRITERION_READ, CRITERION_WRITE, LEVEL_READ, LEVEL_WRITE, ANNOUNCEMENT_READ, ANNOUNCEMENT_WRITE)
+            .stream().collect(Collectors.toSet())),
     STUDENT(Arrays
             .asList(ASSIGNMENT_READ, ASSIGNMENT_WRITE, USER_READ, USER_WRITE, CLASSROOM_READ, TOPIC_READ,
-                    USER_ASSIGNMENT_READ, USER_ASSIGNMENT_WRITE, COMMENT_READ, COMMENT_WRITE, CRITERION_READ, LEVEL_READ)
+                    USER_ASSIGNMENT_READ, USER_ASSIGNMENT_WRITE, COMMENT_READ, COMMENT_WRITE, CRITERION_READ,
+                    LEVEL_READ, ANNOUNCEMENT_READ)
             .stream().collect(Collectors.toSet()));
 
     private final Set<Permissions> permissions;
